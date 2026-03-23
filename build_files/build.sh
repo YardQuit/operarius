6915e41469eec94826f678ca07746b888faed8db
Michael Jones <yardquit@pm.me>
2026-03-23 13:07:25 +0100

    Enhance build script with package installations and cleanup
    Updated build script to include package installations from COPR repositories and added cleanup commands.

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,9 @@
 
 set -ouex pipefail
 
+### Copy sysfiles
+cp -rv /ctx/sysfiles/* /
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -9,16 +12,29 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# This installs a package from fedora repos
+# dnf5 group install --skip-unavailable -y cosmic-desktop
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 install --skip-unavailable -y $(cat /ctx/rpm_packages)
 
-#### Example for enabling a System Unit File
+# Enable copr repositories
+dnf5 -y copr enable atim/starship
+dnf5 -y copr enable pennbauman/ports
 
+# Install from copr repositories
+dnf5 -y install starship
+dnf5 -y install lf
+
+# Disable copr repositories
+dnf5 -y copr disable atim/starship
+dnf5 -y copr disable pennbauman/ports
+
+# Clean
+dnf5 clean all
+
+#### System Unit File
+systemctl enable tuned.service
 systemctl enable podman.socket
+systemctl enable fstrim.timer
+systemctl enable tailscaled.service
+
