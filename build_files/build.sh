ffa9936eb994d5b677693b69cc1f174eb9730bba
Michael Jones <yardquit@pm.me>
2026-05-04 03:34:05 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,9 +20,10 @@ dnf5 install --skip-unavailable -y $(cat /ctx/rpm_packages)
 # Enable copr repositories
 dnf5 -y copr enable atim/starship
 dnf5 -y copr enable pennbauman/ports
-dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# dnf5 -y config-manager addrepo --from-repofile=https://developer.download.nvidia.com/compute/cuda/repos/${distro}/${arch}/cuda-${distro}.repo
+# dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf5 -y config-manager addrepo --from-repofile=https://developer.download.nvidia.com/compute/cuda/repos/${distro}/${arch}/cuda-${distro}.repo
 
 # Enable copr repositories
 # dnf5 -y copr enable atim/starship
@@ -31,9 +32,9 @@ dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release
 # Install from copr repositories
 dnf5 -y install starship
 dnf5 -y install lf
-dnf5 -y install akmod-nvidia
+# dnf5 -y install akmod-nvidia
 
-# dnf5 -y install cuda-drivers
+dnf5 -y install cuda-drivers
 
 # Disable copr repositories
 dnf5 -y copr disable atim/starship
